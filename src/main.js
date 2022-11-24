537a67b51dfe9c8deff8c96df72d54ccc4f6a91f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-24 23:19:17 +0900

    :sparkles:feat: ToDo追加機能実装

--- src/main.js
+++ src/main.js
@@ -1,2 +1,35 @@
 import './css/reset.css';
 import './css/style.css';
+
+document.getElementById('add-button').addEventListener('click', () => {
+  onClickAdd();
+  initValue();
+});
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('input-value').value;
+
+  const li = document.createElement('li');
+  li.className = 'list incomplete-list';
+
+  const p = document.createElement('p');
+  p.className = 'description';
+  p.innerText = inputText;
+
+  const completeButton = document.createElement('button');
+  completeButton.className = 'button complete-button';
+  completeButton.innerText = '完了';
+
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'button delete-button';
+  deleteButton.innerText = '削除';
+
+  li.appendChild(p);
+  document.getElementById('incomplete-lists').appendChild(li);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+};
+
+const initValue = () => {
+  document.getElementById('input-value').value = '';
+};
